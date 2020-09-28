d85ee127da9d2691eb5c4c4a862543d52bd15086
Vincent Buchholz <vincbuchholz@gmail.com>
2020-09-28 14:54:02 +0200

    commit 2

--- Main.java
+++ Main.java
@@ -18,6 +18,8 @@ public class Main {
         Random rand = new Random();
 
         int die1, die2, sum;
+        die1 = 0;
+        die2 = 0;
 
         //System.out.print(terning1 + " " + terning2 + " " + sum);
         int oldScorePlayer1, oldScorePlayer2, newScoreplayer1, newScorePlayer2;
